930e4daa43410a8fce701e2463bbe1159cdcfde9
Samiksha-coder-cloud <78074734+Samiksha-coder-cloud@users.noreply.github.com>
2021-06-02 16:28:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ menu_list_array.sort();
 htmldata="<section class='cards'>"
 for (let i = 0; i <menu_list_array.length; i++) {
     htmldata=htmldata+'<div class="card">'
-    + '<img src="pizzaImg.png"/>'
+    + '<img id="pizza_img" src="pizzaImg.png"/>'
     + menu_list_array[i] + '</div>'
 }
 htmldata=htmldata+"</section>"
